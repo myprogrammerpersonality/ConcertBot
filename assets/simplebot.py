fb11592755d9c566e4da6af6f712bb2e381acca3
Ali_Yazdizadeh <aliyzd1379@gmail.com>
2023-08-20 22:10:05 +0330

    fix group issue

--- assets/simplebot.py
+++ assets/simplebot.py
@@ -1,7 +1,12 @@
 import os
 import requests
 import json
+import logging
 from utils import scrape_webpage
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 TOKEN = os.environ.get('BOT_TOKEN')
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
@@ -14,22 +19,28 @@ def send_message(chat_id, text):
 def lambda_handler(event, context):
     try:
         data = json.loads(event["body"])
+        logger.info(data)
+
         message = str(data["message"]["text"])
         chat_id = data["message"]["chat"]["id"]
-        first_name = data["message"]["chat"]["first_name"]
+        first_name = data["message"]["chat"].get("first_name", "Unknown")
 
 
         if message == "/start":
             response = f"Hello {first_name}"
+            logger.info(response)
         elif message == "/scrape":
             response = scrape_webpage()
             for result in response:
                 send_message(chat_id, str(result))
             response = f"Sent {len(response)} events!"
+            logger.info(response)
         elif message == "/info":
             response = f"Your Chat ID is {chat_id}"
+            logger.info(response)
         else:
             response = "Please use /start or /scrape"
+            logger.info(response)
 
         send_message(chat_id, response)
 
