1561f1b06d4712dce8c474c217b43b6c2a7c2d62
Ali_Yazdizadeh <aliyzd1379@gmail.com>
2023-08-20 22:46:49 +0330

    apply daily rule

--- stacks/bot_stack.py
+++ stacks/bot_stack.py
@@ -63,7 +63,11 @@ class BotStack(Stack):
         rule = events.Rule(
             self,
             "Rule",
-            schedule=events.Schedule.rate(Duration.minutes(5)), # Trigger every 5 minutes
+            schedule=events.Schedule.cron(
+                minute="30",  # At minute 30
+                hour="6",  # At 6:00 AM UTC (10:00 AM +3:30 Tehran time)
+                # Note: The time zone of the schedule is UTC by default
+            ),
         )
 
         # Add default data for the trigger
